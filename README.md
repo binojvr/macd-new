e6ff6e708beccd7098bb7b912a61df1b26525442
binojvr <binojvr@yahoo.com>
2023-01-07 20:16:18 +0900

    comment added

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ the buying and selling points in the graph.
 with the help of Numpy, panadas and matplotlib libraries of Python. 
 Used Moving Average Convergence Divergence (MACD) to analyse 
 the data and set the buying and selling point in the graph. 
-with the help of exponential moving average (EMA) plotted the 
+with the help of moving average (SMA) plotted the 
 point so that user can easily buy and sell the shares.
--- macd.py
+++ macd.py
@@ -2,15 +2,17 @@ import pandas as pd
 import yfinance as yf
 import numpy as np
 
+SIGNAL_WINDOW = 9
 SHORT_WINDOW = 12
 LONG_WINDOW = 26
-SIGNAL_WINDOW = 9
 
 
+# find the moving average for specific window size(number of rows)
 def mov_avg(df: pd.DataFrame(), w, n):
     df[n] = df['Close'].rolling(window=w).mean()
 
 
+# find macd and signal line
 def macd(df: pd.DataFrame()):
     df['MACD'] = df['slowma'] - df['fastma']
     mov_avg(df, SIGNAL_WINDOW, 'signal')
@@ -20,6 +22,17 @@ def macd(df: pd.DataFrame()):
 def buy_sell_signal(df: pd.DataFrame()):
     df['buy'] = np.where(df['MACD'] > df['signal'], 1.0, 0.0)
     df['sell'] = np.where(df['MACD'] < df['signal'], 1.0, 0.0)
+
+
+"""
+　get the data for specific symbol
+  drop few columns which are not required for macd
+  find slow moving average (12 period)
+  find fast moving average (26 period)
+  calculate macd
+  find signal line from 9 period moving average of macd
+  get the buy and sell signal from macd and signal
+"""
 
 
 def main(sym):
